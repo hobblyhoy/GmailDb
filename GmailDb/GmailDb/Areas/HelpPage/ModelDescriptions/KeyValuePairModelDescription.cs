2c08f67d30a22cb9b716709a0c509f553b683d43
hobblyhoy <hobblyhoy@gmail.com>
2018-03-12 16:51:15 -0700

    init

---
+++ GmailDb/GmailDb/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace GmailDb.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
